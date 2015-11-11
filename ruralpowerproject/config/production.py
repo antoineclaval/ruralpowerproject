0b977c3a3686c2a032d3b75495c96df8a500ce6b
antoineclaval <antoine.claval@gmail.com>
2015-11-11 14:39:03 -0600

    dont need sendgrid for now, comment that out

--- ruralpowerproject/config/production.py
+++ ruralpowerproject/config/production.py
@@ -98,14 +98,14 @@ class Production(Common):
     # END STORAGE CONFIGURATION
 
     # EMAIL
-    DEFAULT_FROM_EMAIL = values.Value('ruralpowerproject <noreply@ruralpowerproject.org>')
-    EMAIL_HOST = values.Value('smtp.sendgrid.com')
-    EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
-    EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
-    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
-    EMAIL_SUBJECT_PREFIX = values.Value('[ruralpowerproject] ', environ_name="EMAIL_SUBJECT_PREFIX")
-    EMAIL_USE_TLS = True
-    SERVER_EMAIL = EMAIL_HOST_USER
+    # DEFAULT_FROM_EMAIL = values.Value('ruralpowerproject <noreply@ruralpowerproject.org>')
+    # EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    # EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    # EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    # EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    # EMAIL_SUBJECT_PREFIX = values.Value('[ruralpowerproject] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    # EMAIL_USE_TLS = True
+    # SERVER_EMAIL = EMAIL_HOST_USER
     # END EMAIL
 
     # TEMPLATE CONFIGURATION
